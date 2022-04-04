50d4aece9f300988eda08279751e6190da8f927e
Daryl Rogony <darylrogony@gmail.com>
2022-04-04 14:25:11 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,30 @@
+function add(num1,num2) {
+  return num1 + num2
+}
+
+function subtract(num1,num2) {
+  return num1 - num2
+}
+
+function divide(num1,num2) {
+  return num1 / num2
+}
+
+function multiply(num1,num2) {
+  return num1 * num2
+}
+
+function increment(x) {
+  return x +=1
+}
+function decrement(x) {
+  return x -= 1
+}
+
+function makeInt(string) {
+  return parseInt(string, 10)
+}
+
+function preserveDecimal(string) {
+  return parseFloat(string)
+}
